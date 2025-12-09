e2a74ecdceb6b499eafa7472f0f504111085faf3
Libin <libinthankayathil@gmail.com>
2025-12-09 20:25:48 +0530

    add zod validation

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { addToWatchlist, removeFromWatchlist, updateWatchlistItem } from '../controllers/watchlistController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { validateRequest } from '../middlewares/validateRequest.js';
+import { addToWatchlistSchema } from '../validators/watchlistValidators.js';
 
 const router = express.Router()
 
 router.use(authMiddleware)
 
-router.post("/", addToWatchlist);
+router.post("/", validateRequest(addToWatchlistSchema), addToWatchlist);
 
 router.put("/:id", updateWatchlistItem);
 
